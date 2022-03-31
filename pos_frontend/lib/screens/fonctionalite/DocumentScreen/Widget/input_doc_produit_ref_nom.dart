c54bee27401b804474b9ca2b39a2df55006db92e
Karim Hachicha <karim.hachicha6822@yahoo.fr>
2022-03-31 09:56:34 +0100

    Recherche Produit en Ligne Document

--- pos_frontend/lib/screens/fonctionalite/DocumentScreen/Widget/input_doc_produit_ref_nom.dart
+++ pos_frontend/lib/screens/fonctionalite/DocumentScreen/Widget/input_doc_produit_ref_nom.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:searchfield/searchfield.dart';
 
-class InputRefNomProduit extends StatelessWidget {
+class InputRefNomProduit extends StatefulWidget {
   final String label, label2, label3, label4;
   final String content, content2, content3, content4;
-  var fieldController = TextEditingController();
   var fieldController2 = TextEditingController();
   FormFieldValidator<String> fieldValidator = (_) {};
   InputRefNomProduit({
@@ -20,6 +22,46 @@ class InputRefNomProduit extends StatelessWidget {
   });
 
   @override
+  State<InputRefNomProduit> createState() => _InputRefNomProduitState();
+}
+
+class _InputRefNomProduitState extends State<InputRefNomProduit> {
+  var fieldController = TextEditingController();
+  String selectedProduit;
+  int produitId;
+  List produits = [];
+  List<String> refProduits = [];
+  @override
+  void initState() {
+    super.initState();
+    this.fetchProduits();
+  }
+
+  fetchProduits() async {
+    final response = await http.get(
+      Uri.parse('http://127.0.0.1:8000/api/produit'),
+      headers: <String, String>{
+        'Cache-Control': 'no-cache',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      var items = jsonDecode(response.body);
+      setState(() {
+        produits = items;
+        print(produits);
+      });
+    } else {
+      throw Exception('Error!');
+    }
+    for (var i = 0; i < produits.length; i++) {
+      setState(() {
+        refProduits.add(produits[i]['refProd']);
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (BuildContext context, BoxConstraints constraints) {
@@ -29,7 +71,7 @@ class InputRefNomProduit extends StatelessWidget {
               flex: 2,
               child: Container(
                 child: Text(
-                  "$label",
+                  "${widget.label}",
                   textAlign: TextAlign.left,
                   style: TextStyle(
                     fontWeight: FontWeight.w900,
@@ -46,21 +88,26 @@ class InputRefNomProduit extends StatelessWidget {
               child: Container(
                 width: MediaQuery.of(context).size.width / 3.7,
                 color: Color.fromARGB(255, 255, 255, 255),
-                child: TextFormField(
+                child: SearchField(
+                  hint: "${widget.content}",
+                  searchStyle: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
                   controller: fieldController,
-                  validator: fieldValidator,
-                  style: TextStyle(
-                    fontSize: 15.0,
-                    color: Colors.black,
-                  ),
-                  decoration: InputDecoration(
+                  validator: widget.fieldValidator,
+                  searchInputDecoration: InputDecoration(
                     contentPadding: EdgeInsets.all(10.0),
-                    hintText: "$content",
-                    hintStyle: TextStyle(
-                        color: Color.fromARGB(255, 190, 190, 190),
-                        fontSize: 14),
-                    fillColor: Color.fromARGB(255, 0, 0, 0),
                   ),
+                  suggestions: refProduits,
+                  suggestionStyle:
+                      TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                  maxSuggestionsInViewPort: 6,
+                  suggestionsDecoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  onTap: (value) {
+                    setState(() {
+                      selectedProduit = value as String;
+                    });
+                  },
                 ),
               ),
             ),
@@ -72,7 +119,7 @@ class InputRefNomProduit extends StatelessWidget {
               child: Container(
                 width: 50.0,
                 child: Text(
-                  "$label2",
+                  "${widget.label2}",
                   textAlign: TextAlign.left,
                   style: TextStyle(
                     fontWeight: FontWeight.w900,
@@ -92,14 +139,14 @@ class InputRefNomProduit extends StatelessWidget {
                 child: TextFormField(
                   enabled: false,
                   controller: fieldController,
-                  validator: fieldValidator,
+                  validator: widget.fieldValidator,
                   style: TextStyle(
                     fontSize: 15.0,
                     color: Colors.black,
                   ),
                   decoration: InputDecoration(
                     contentPadding: EdgeInsets.all(10.0),
-                    hintText: "$content2",
+                    hintText: "${widget.content2}",
                     hintStyle: TextStyle(
                         color: Color.fromARGB(255, 190, 190, 190),
                         fontSize: 14),
@@ -116,7 +163,7 @@ class InputRefNomProduit extends StatelessWidget {
               child: Container(
                 width: 50.0,
                 child: Text(
-                  "$label3",
+                  "${widget.label3}",
                   textAlign: TextAlign.left,
                   style: TextStyle(
                     fontWeight: FontWeight.w900,
@@ -132,14 +179,14 @@ class InputRefNomProduit extends StatelessWidget {
                 color: Color.fromARGB(255, 255, 255, 255),
                 child: TextFormField(
                   controller: fieldController,
-                  validator: fieldValidator,
+                  validator: widget.fieldValidator,
                   style: TextStyle(
                     fontSize: 15.0,
                     color: Colors.black,
                   ),
                   decoration: InputDecoration(
                     contentPadding: EdgeInsets.all(10.0),
-                    hintText: "$content3",
+                    hintText: "${widget.content3}",
                     hintStyle: TextStyle(
                         color: Color.fromARGB(255, 190, 190, 190),
                         fontSize: 14),
@@ -155,7 +202,7 @@ class InputRefNomProduit extends StatelessWidget {
               flex: 1,
               child: Container(
                 child: Text(
-                  "$label4",
+                  "${widget.label4}",
                   textAlign: TextAlign.left,
                   style: TextStyle(
                     fontWeight: FontWeight.w900,
@@ -172,14 +219,14 @@ class InputRefNomProduit extends StatelessWidget {
                 child: TextFormField(
                   enabled: true,
                   controller: fieldController,
-                  validator: fieldValidator,
+                  validator: widget.fieldValidator,
                   style: TextStyle(
                     fontSize: 15.0,
                     color: Colors.black,
                   ),
                   decoration: InputDecoration(
                     contentPadding: EdgeInsets.all(10.0),
-                    hintText: "$content4",
+                    hintText: "${widget.content4}",
                     hintStyle: TextStyle(
                         color: Color.fromARGB(255, 190, 190, 190),
                         fontSize: 14),
