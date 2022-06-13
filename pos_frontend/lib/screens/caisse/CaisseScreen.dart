4479048493e72fd65f064cc2729885a5208a10e8
Karim Hachicha <karim.hachicha6822@yahoo.fr>
2022-06-13 16:50:41 +0100

    Interface Screen Liste Produits

--- pos_frontend/lib/screens/caisse/CaisseScreen.dart
+++ pos_frontend/lib/screens/caisse/CaisseScreen.dart
@@ -1,7 +1,9 @@
+import 'package:admin/responsive.dart';
 import 'package:admin/screens/dashboard/components/header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flip_card/flip_card.dart';
 
 class Caisse extends StatefulWidget {
   Caisse({Key? key}) : super(key: key);
@@ -11,6 +13,7 @@ class Caisse extends StatefulWidget {
 }
 
 class _CaisseState extends State<Caisse> {
+  int? quantite;
   int selectedTable = 1;
   @override
   Widget build(BuildContext context) {
@@ -66,8 +69,8 @@ class _CaisseState extends State<Caisse> {
               Align(
                 alignment: Alignment.topRight,
                 child: SizedBox(
-                  height: 450,
-                  width: 275,
+                  height: MediaQuery.of(context).size.height / 2.5,
+                  width: MediaQuery.of(context).size.width / 7,
                   child: Container(
                     padding: const EdgeInsets.all(20),
                     decoration: BoxDecoration(
@@ -140,8 +143,8 @@ class _CaisseState extends State<Caisse> {
                 child: Padding(
                   padding: const EdgeInsets.only(top: 25),
                   child: Container(
-                    width: 275,
-                    height: 410,
+                    width: MediaQuery.of(context).size.width / 7,
+                    height: MediaQuery.of(context).size.height / 2.5,
                     decoration: BoxDecoration(
                       color: Color(0xFF462c86),
                       borderRadius: BorderRadius.circular(25),
@@ -154,20 +157,121 @@ class _CaisseState extends State<Caisse> {
           Padding(
             padding: const EdgeInsets.only(left: 25, bottom: 20),
             child: Container(
-              width: 1000,
-              decoration: BoxDecoration(
-                color: Color(0xFF462c86),
-                borderRadius: BorderRadius.circular(25),
-              ),
-            ),
+                width: MediaQuery.of(context).size.width / 2,
+                decoration: BoxDecoration(
+                  color: Color(0xFF462c86),
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: GridView.count(
+                  crossAxisCount: 4,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                  primary: false,
+                  padding: EdgeInsets.all(20),
+                  children: <Widget>[
+                    FlipCard(
+                      direction: FlipDirection.HORIZONTAL,
+                      front: Container(
+                        height: MediaQuery.of(context).size.width * 2,
+                        width: MediaQuery.of(context).size.height * 2,
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black,
+                              offset: Offset(0.0, 1.0), //(x,y)
+                              blurRadius: 1,
+                            ),
+                          ],
+                          color: Color(0xFF553b93),
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(25),
+                                  topRight: Radius.circular(25)),
+                              child: Image.asset(
+                                "assets/images/jus.png",
+                                //height: MediaQuery.of(context).size.height / 3,
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 2, left: 20),
+                              child: Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  "Jus d'orange",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 2, left: 20),
+                              child: Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  "2 DT",
+                                  style: TextStyle(
+                                    color: Colors.grey[300],
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      back: Container(
+                          height: 75,
+                          width: 75,
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black,
+                                offset: Offset(0.0, 1.0), //(x,y)
+                                blurRadius: 1,
+                              ),
+                            ],
+                            color: Colors.grey[800],
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Quantite(),
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    primary: Colors.grey[800],
+                                    fixedSize: Size(200, 40),
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(25)),
+                                    side: BorderSide(
+                                      color: Colors.white,
+                                    )),
+                                onPressed: () {},
+                                child: Text("Ajouter au Panier",
+                                    style: TextStyle(fontSize: 16)),
+                              ),
+                            ],
+                          )),
+                    ),
+                  ],
+                )),
           ),
           Align(
             alignment: Alignment.topLeft,
             child: Padding(
               padding: const EdgeInsets.only(left: 25, bottom: 20),
               child: Container(
-                width: 450,
-                height: 500,
+                width: MediaQuery.of(context).size.width / 5,
+                height: MediaQuery.of(context).size.height / 1,
                 decoration: BoxDecoration(
                   color: Color(0xFF462c86),
                   borderRadius: BorderRadius.circular(25),
@@ -177,6 +281,48 @@ class _CaisseState extends State<Caisse> {
           ),
         ],
       ),
+    );
+  }
+
+  Row Quantite() {
+    quantite = 0;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              primary: Colors.grey[800],
+              fixedSize: Size(40, 40),
+              shape: CircleBorder(),
+              side: BorderSide(
+                color: Colors.white,
+              )),
+          onPressed: () {
+            if (quantite! > 0) {
+              setState(() {
+                quantite = quantite! - 1;
+              });
+            }
+          },
+          child: Icon(Icons.remove),
+        ),
+        Text('$quantite', style: TextStyle(fontSize: 36)),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              primary: Colors.grey[800],
+              fixedSize: Size(40, 40),
+              shape: CircleBorder(),
+              side: BorderSide(
+                color: Colors.white,
+              )),
+          onPressed: () {
+            setState(() {
+              quantite = quantite! + 1;
+            });
+          },
+          child: Icon(Icons.add),
+        ),
+      ],
     );
   }
 }
